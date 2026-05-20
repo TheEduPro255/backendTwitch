fc7b42dcfdd281d1b2179dc5dda82a1489eabd4e
Dark Craft <a20ruloed@iesgrancapitan.org>
2026-05-20 18:33:08 +0200

    commit2

--- server.js
+++ server.js
@@ -5,14 +5,19 @@ const axios = require("axios");
 
 const app = express();
 
+
+
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const PORT = process.env.PORT || 3000;
+const redirect_uri = "https://twitchappbackend-1.onrender.com/callback";
+
 // const scope = "user:read:email";
 
 
 app.get("/", async (req, res) => {
 
+    console.log("Entra en /");
     try {
 
         const tokenResponse = await axios.post(
@@ -45,7 +50,7 @@ app.get("/", async (req, res) => {
 
     } catch (err) {
 
-        console.error(err.response?.data || err.message);
+        console.error(err.response?.data  || err.message);
 
         res.status(500).json({
             error: "Algo salió mal"
@@ -56,20 +61,36 @@ app.get("/", async (req, res) => {
 
 app.get("/login", (req, res) => {
     const client_id = process.env.CLIENT_ID;
-    const redirect_uri = "https://twitchappbackend-1.onrender.com/callback";
     const scope = "user:read:email";
+        console.log("Entra en /login");
+
 
     const authUrl =
         `https://id.twitch.tv/oauth2/authorize` +
         `?client_id=${client_id}` +
-        `&redirect_uri=${encodeURIComponent(redirect_uri)}` +
-        `&response_type=code` +
-        `&scope=${encodeURIComponent(scope)}`;
-
-    res.redirect(authUrl);
+        `&redirect_uri=${redirect_uri}` +
+        `&response_type=code `+
+        `&scope=${scope}`+
+        `&force_verify=true`;
+res.send(
+        <html>
+        <head>
+            <title>Login Twitch</title>
+        </head>
+        <body>
+            <h1>Login con Twitch</h1>
+            <a href="${authUrl}">
+                <button>Iniciar sesión</button>
+            </a>
+        </body>
+        </html>
+    );
 });
 
+
 app.get("/callback", async (req, res) => {
+        console.log("Entra en /callback");
+
     const code = req.query.code;
 
     try {
@@ -86,7 +107,29 @@ app.get("/callback", async (req, res) => {
 
         const accessToken = tokenResponse.data.access_token;
 
-        res.redirect("pruebasapp://auth?token=" + accessToken);
+
+res.send(
+<html>
+<head>
+    <title>Login completado</title>
+</head>
+<body>
+    <h1>✅ Login Twitch correcto</h1>
+    <p>Pulsa para volver a la app</p>
+
+    <a href="pruebasapp://auth?token=${accessToken}">
+        <button>Volver a la app</button>
+    </a>
+
+    <script>
+        // intento automático
+        window.location.href = "pruebasapp://auth?token=${accessToken}";
+    </script>
+</body>
+</html>
+);
+
+ 
 
     } catch (err) {
         console.error(err.response?.data || err.message);
@@ -101,4 +144,3 @@ app.get("/callback", async (req, res) => {
 app.listen(PORT, () => {
     console.log("Servidor iniciado en puerto "+PORT);
 });
-
